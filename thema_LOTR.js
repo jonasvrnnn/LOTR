30bf66b7035ce01093c769042a2f49bfa370c02d
Najlae Aanannaz <najlae.aanannaz@student.ap.be>
2025-03-10 10:24:40 +0000

    nieuwe bestanden

---
+++ thema_LOTR.js
@@ -0,0 +1,10 @@
+function switchTheme(theme) {
+    document.body.className = theme; 
+}
+
+//Wisselen naar lichte thema
+switchTheme('light');
+
+//Wisselen naar donkere thema
+switchTheme('dark');
+
